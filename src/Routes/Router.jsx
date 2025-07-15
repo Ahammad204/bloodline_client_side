9372a8db7141ef625cfc3d686accda7677ba3a32
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-07-15 10:39:49 +0600

    Add Error page component and integrate it into the router for handling 404 errors

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,11 +3,13 @@ import Home from "../Home/Home/Home";
 import Main from "../Main/Main";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import Error from "../Pages/Error/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <Error></Error>,
     children: [
       {
         path:"/",
