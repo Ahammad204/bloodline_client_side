76448e14cc4f67028a0d1b6888102a7a5b91cfde
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-07-15 10:46:44 +0600

    Add Featured section with icons and integrate into Home component

--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import FeaturedSection from '../Featured/Featured';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
+            <FeaturedSection></FeaturedSection>
         </>
     );
 };
